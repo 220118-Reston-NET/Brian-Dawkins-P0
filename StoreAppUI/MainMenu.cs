37c6652e420b596213b4ae0b08b6cedc25d58a7f
Brian Dawkins <brian.dawkins@reature.net>
2022-01-25 12:32:50 -0500

    MainMenu created

---
+++ StoreAppUI/MainMenu.cs
@@ -0,0 +1,46 @@
+namespace StoreUI
+{
+    /*
+    MainMenu inherits IMenu interface but since it is a class it needs to give actual implementation details to the methods
+    stated inside of the interface
+    */
+
+    public class MainMenu : IMenu
+    {
+        public void Display()
+        {
+            Console.WriteLine("Welcome to The Shop Memorabilia Store");
+            Console.WriteLine(" What you like to do?");
+            Console.WriteLine("[1] Check available inventory");
+            Console.WriteLine("[2] Shop");
+            Console.WriteLine("[3] See previous order");
+            Console.WriteLine("[4] Find Location");
+            Console.WriteLine("[0] Exit");
+        }
+
+        public string UserChoice()
+        {
+            string userInput = Console.ReadLine();
+
+            //Switch cases useful if doing a bunch of comparison
+            switch (userInput)
+            {
+                case "0":
+                    return "Exit";
+                case "1":
+                    return "Check Inventory";
+                case "2":
+                    return "Shop";
+                case "3":
+                    return "Previous Order";
+                case "4":
+                    return "Find Location";
+                default:
+                    Console.WriteLine("Please input a valid response");
+                    Console.WriteLine("Please press Enter to continue");
+                    Console.ReadLine();
+                    return "Main Menu";                    
+            }
+        }
+    }
+}
