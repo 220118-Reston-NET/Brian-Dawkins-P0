01512e2cb04d68adfcaf2e82611a678cb020e660
Brian Dawkins <brian.dawkins@reature.net>
2022-01-25 12:47:22 -0500

    Data layer start

---
+++ StoreAppDL/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using StoreAppModel;
+
+namespace StoreAppDL
+{
+    /// <summary>
+    ///- Data layer project is responsible for interacting with our database and doing CRUD operations
+    ///- C - create, R - Read, U - Update, D - Delete
+    ///- It must not have logical operation that will also manipulate the data it is grabbing
+    ///- Just think of Data layer as the delivery man of your uber eats. You definitely dont want your delivery man to touch
+    /// the food he is delivering and just give it to you so you can do whatever you want with it.   
+    /// </summary>
+public interface IRepository
+{
+    
+    
+}
+}
