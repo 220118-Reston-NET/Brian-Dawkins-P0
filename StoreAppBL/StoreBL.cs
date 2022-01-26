160b55c66da629ed2618a0552c759b9f07887b8c
Brian Dawkins <brian.dawkins@reature.net>
2022-01-26 11:17:45 -0500

    Business Layer Creation

---
+++ StoreAppBL/StoreBL.cs
@@ -0,0 +1,32 @@
+using StoreAppDL;
+using StoreFrontModel;
+
+namespace StoreBL
+{
+    public class StoreBL : IStoreBL
+    {
+        //Dependency Injection Pattern
+        //- This is the main reason why we created interface first before the class
+        //- This will save you time on re-writing code that breaks if you updated a completely seperate class
+        //- Main reason is to prevent us from re-writing code that already existed on (potentially) 50 file or more without
+        //the compiler helping us
+        //===========================
+        private IRepository _repo;
+        public StoreBL(IRepository p_repo)
+        {
+            repo = p_repo;
+        }
+        //===========================
+
+        public Customer AddCustomer(Customer c_customer)
+        {
+            Random rand = new Random();
+
+            //Processing data to meet conditions
+            //Not sure if this step is needed for adding customers
+
+            //Validation Process
+            
+        }
+    }
+}
