919e8c5e2841df0b5f87e341e5cbfcb805252d77
Brian Dawkins <brian.dawkins@reature.net>
2022-01-26 13:02:19 -0500

    AddCustomer changes

--- StoreAppUI/AddCustomer.cs
+++ StoreAppUI/AddCustomer.cs
@@ -19,7 +19,29 @@ namespace StoreUI
 
         public void Display()
         {
-            
+            Console.WriteLine("Enter Customer Information");
+            Console.WriteLine("[3] Name - " + _newCustomer.Name);
+            Console.WriteLine("[2] Address - " + _newCustomer.Address);
+            Console.WriteLine("[1] Save");
+            Console.WriteLine("[0] Go Back");
+        }
+
+        public string UserChoice()
+        {
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "0":
+                    return "MainMenu";
+                case "1":
+                    //Exception handling to have better user experience 
+                    try
+                    {
+                        _customerBL.AddCustomer(_newCustomer);
+                    } 
+                    catch (System.Exception exc)
+            }
         }
     }
 }
