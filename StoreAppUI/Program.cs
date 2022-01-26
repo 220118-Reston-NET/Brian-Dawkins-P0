52b6a57c8e2a2516522a1cf13de076060e480fce
Brian Dawkins <brian.dawkins@reature.net>
2022-01-26 15:30:39 -0500

    Store App UI fix

--- StoreAppUI/Program.cs
+++ StoreAppUI/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//using CustomerModel;
+using StoreBL;
+using StoreAppDL;
+using StoreUI;
+
+//Console.WriteLine("Hello, World!");
+
+bool repeat = true;
+IMenu menu = new MainMenu();
+
+while(repeat)
+{
+    Console.Clear();
+    menu.Display();
+    string ans = menu.UserChoice();
+
+    switch (ans)
+    {
+        case "AddCustomer":
+            menu = new AddCustomer(new StoreFrontBL(new Repository()));
+            break;
+        case "MainMenu":
+            menu = new MainMenu();
+            break;
+        case "Exit":
+            repeat = false;
+            break;
+        default:
+            Console.WriteLine("Page does not exist!");
+            break;
+    }
+}
