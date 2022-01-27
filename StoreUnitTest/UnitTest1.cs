116230718490562b76c7e5ef7f0c42343a26c420
Brian Dawkins <brian.dawkins@reature.net>
2022-01-27 15:27:21 -0500

    CustomerTest added

--- StoreUnitTest/UnitTest1.cs
+++ StoreUnitTest/UnitTest1.cs
@@ -1,12 +1,28 @@
 using Xunit;
+using CustomerModel;
 
 namespace StoreUnitTest;
 
-public class UnitTest1
+public class CustomerTest
 {
+    /// <summary>
+    /// Checks the validation for Name property for valid data
+    /// </summary>
+    /// [Fact] is a data annotation in C# and all it means is it will tell the compiler that this specific method is a unit test
     [Fact]
-    public void Test1()
+    public void NameShouldSetValidData()
     {
+        //Arrange
+        Customer cus = new Customer();
+        string validName = "Brian Dawkins";
+        
+
+        //Act
+        cus.Name = validName;
+
+        //Assert
+        Assert.NotNull(cus.Name); //checks that the property is not null meaning we did set data in this property
+        Assert.Equal(validName, cus.Name); //checks if the property does indeed hold the same value as what we set it as
 
     }
 }
