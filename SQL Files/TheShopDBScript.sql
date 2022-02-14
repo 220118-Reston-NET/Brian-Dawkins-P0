3f977a17a8216224d464b852383a970a7609ede2
Brian Dawkins <brian.dawkins@reature.net>
2022-02-14 10:02:54 -0500

    updates

--- SQL Files/TheShopDBScript.sql
+++ SQL Files/TheShopDBScript.sql
@@ -17,7 +17,15 @@ create table ListOfOrder(
 );
 
 --Ends my tables that belong to my customer model
-
+insert into customer 
+values ('Brian Dawkins', '5058 Brighton Drive'),
+	('Kayla Shirk', '1909 University Blvd S')
+	
+insert into phoneNumber 
+values ('904-654-3613'),
+	('904-535-6961')
+	
+select * from customer 
 
 --Starts the relationship tables between Customer, phonenumber, and ListOfOrder 
 --Many to Many between customer and phonenumber (Spouses could have same phone number)
@@ -26,6 +34,9 @@ create table customer_phonenumbers(
 	phoneNumberId int foreign key references phoneNumber(phoneNumberId)
 );
 
+
+	
+select * from customer_phonenumbers cp 
 --One to Many relationship between customer and list of orders
 alter table ListOfOrder 
 add customerId int foreign key references Customer(CustomerId)
@@ -42,29 +53,53 @@ create table Orders(
 	total int 
 );
 
+select * from Orders o 
+
+insert into Orders (total)  
+values (45)
+	
+	
 create table Location(
 	StoreId int identity (1,1) primary key,
 	LName varchar(50),
 	LAddress varchar(50),
 );
 
-create table LineItems(
-	ItemId int identity (1,1) primary key,
-	Product varchar(50)
-);
+insert into Location 
+values ('Southside', '2431 Southside Blvd'),
+	('Northside', '5679 Northside Blvd'),
+	('Eastside', '1267 Eastside Blvd'),
+	('Westside', '4387 Westside Blvd')
 
+create table LineItems(
+	ProductId int foreign key references Product(ProductId),
+	OrderId int foreign key references Orders(OrderId),
+	Quantity int 
+)
+alter table LineItems 
+add Quantity int
+
+select * from LineItems li 
+insert into LineItems 
+values ('Jerseys', 50),
+	('Helmets', 50),
+	('Shoes', 60)
+	
 --Ends my tables that belong to my Order Model 
 
 --Starts the relationship tables between Orders, Locations, and LineItems 
 --One to many (One store can have many Orders)
 alter table Orders 
-add StoreId int foreign key references Location(StoreId)
+add StoreId int identity (1,1) foreign key references Location(StoreId)
 
 --Many to Many (Many orders can have many Line items)
 create table Orders_LineItems(
 	OrderId int foreign key references Orders(OrderId),
 	ItemId int foreign key references LineItems(ItemId)
 )
+
+insert into Orders_LineItems 
+values (3,2)
 
 --Many Locations can have many Line Items
 create table Locations_LineItems(
@@ -74,6 +109,9 @@ create table Locations_LineItems(
 
 --Where our insert statements go for values 
 
+select * from Orders o
+inner join Orders_LineItems oli on o.OrderId = oli.OrderId 
+inner join LineItems li on li.ItemId = oli.ItemId 
 --Ends the relationship tables between Orders, Locations, and LineItems 
 
 --Relationship between customers and locations
@@ -88,6 +126,24 @@ create table customers_locations(
 alter table Orders 
 add CustomerId int foreign key references Customer(CustomerId)
 
+select c.CustomerId, o.OrderId, o.total  from Orders o  
+inner join customer c on o.CustomerId  = c.CustomerId  
 
+select c.CustomerId, oli.OrderId from Orders_LineItems oli 
+
+create table Product(
+	ProductId int identity (1,1) primary key,
+	Name varchar(50),
+	Price int,
+	Category varchar(50)
+)
+
+create table Inventory(
+	ProductId int foreign key references Product(ProductId),
+	StoreId int foreign key references Location(StoreId),
+	Quantity int
+)
+
+select * from Inventory i 
 
 
